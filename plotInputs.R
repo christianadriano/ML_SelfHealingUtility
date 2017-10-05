4ac5bbd6626d444455df27b6b025444f0c094dff
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-10-05 21:01:37 +0200

    change axis for the plots

--- plotInputs.R
+++ plotInputs.R
@@ -1,21 +1,22 @@
-#Plot the inputs (CRITICALITY	CONNECTIVITY	RELIABILITY, UTILITY.DROP)
-#How are the features distributed?
-#How are the features correlated?
+#Plot the inputs (CRITICALITY,	CONNECTIVITY	RELIABILITY, UTILITY.DROP)
+#How are the features distributed? Right skewed.
+#How are the features correlated? Yes, medium to highly correlated.
 
 library(ggplot2)
 
 plotInputs<- function(fileName){
   
-  fileName<-"failureRules_data.csv";
+  fileName<-"MLDATA2_data.csv";
+  fileName<-"MLDATA2_STATIC.csv";
   setwd("C://Users//chris//OneDrive//Documentos//GitHub//ML_SelfHealingUtility//");
   
   data_all <- read.csv(fileName,header = TRUE,sep=",");
   
   dataf <- data.frame(data_all);
-  summary(dataf)
+  summary(dataf);
   
   ggplot(data=dataf, aes(x=dataf$UTILITY.DROP)) +
-    geom_histogram(binwidth = 0.5,alpha=.2, position="identity")+
+    geom_bar(alpha=.2, position="identity")+
     geom_vline(aes(xintercept=mean(dataf$UTILITY.DROP, na.rm=T)),   # Ignore NA values for mean
                color="red", linetype="dashed", size=1) +
     ggtitle("Utility drop distribution")+
@@ -23,20 +24,33 @@ plotInputs<- function(fileName){
          y="Frequency");
   
   ggplot(data=dataf, aes(x=dataf$CRITICALITY)) +
-    geom_histogram(binwidth = 0.2,alpha=.2, position="identity")+
+    geom_bar(alpha=.2, position="identity")+
     geom_vline(aes(xintercept=mean(dataf$CRITICALITY, na.rm=T)),   # Ignore NA values for mean
                color="red", linetype="dashed", size=1) +
     ggtitle("Component criticality distribution")+
     labs(x="Criticality of the component", 
-         y="Frequency");
+         y="Frequency")+
+    scale_x_discrete(limits=c(min(dataf$CRITICALITY):max(dataf$CRITICALITY)));
  
   ggplot(data=dataf, aes(x=dataf$RELIABILITY)) +
     geom_histogram(binwidth = 0.1,alpha=.2, position="identity")+
     geom_vline(aes(xintercept=mean(dataf$RELIABILITY, na.rm=T)),   # Ignore NA values for mean
                color="red", linetype="dashed", size=1) +
     ggtitle("Component reliability distribution")+
-    labs(x="Criticality of the component", 
-         y="Frequency");
+    labs(x="Reliability of the component", 
+         y="Frequency")+
+    scale_x_discrete(limits=c(min(dataf$RELIABILITY):max(dataf$RELIABILITY)));
+  
+  ggplot(data=dataf, aes(x=dataf$CONNECTIVITY)) +
+    geom_bar(alpha=.2, position="identity")+
+    geom_vline(aes(xintercept=mean(dataf$CONNECTIVITY, na.rm=T)),   # Ignore NA values for mean
+               color="red", linetype="dashed", size=1) +
+    ggtitle("Component Connectivity distribution")+
+   labs(x="Connectivity of the component", 
+         y="Frequency")+
+    scale_x_discrete(limits=c(min(dataf$CONNECTIVITY):max(dataf$CONNECTIVITY)));
+
+#    
   
   #Correlations
   # Data is not normal
@@ -49,11 +63,11 @@ plotInputs<- function(fileName){
   colnames(matrixInput)<-c("Utility_Drop","Criticality","Connectivity","Reliability");
   
   cor(matrixInput, method="kendall",use="pairwise");
-  #               Utility_Drop Criticality Connectivity Reliability
-  # Utility_Drop    1.0000000   0.8496351    0.9023943   0.3956244
-  # Criticality     0.8496351   1.0000000    0.6542992   0.4622369
-  # Connectivity    0.9023943   0.6542992    1.0000000   0.3286646
-  # Reliability     0.3956244   0.4622369    0.3286646   1.0000000
+#                 Utility_Drop Criticality Connectivity Reliability
+#  Utility_Drop    1.0000000   0.6989180    0.4979811   0.3579032
+#  Criticality     0.6989180   1.0000000    0.1198513   0.3172049
+#  Connectivity    0.4979811   0.1198513    1.0000000   0.3265254
+#  Reliability     0.3579032   0.3172049    0.3265254   1.0000000
   
   
   #As we can see that the data is medium to strongly correlated
