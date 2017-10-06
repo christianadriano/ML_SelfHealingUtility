bf3d6fcc2b5c1ff07165b42e0a6d8f64b1855b97
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-10-06 15:38:03 +0200

    added remove methods

--- loadData.R
+++ loadData.R
@@ -1,5 +1,6 @@
-#Load all data into a dataframe
 
+#---------------------------------------------------------------
+#Load all data into a dataframe
 loadData<- function(fileName){
   
   setwd("C://Users//chris//OneDrive//Documentos//GitHub//ML_SelfHealingUtility//");
@@ -7,8 +8,32 @@ loadData<- function(fileName){
   data_all <- read.csv(fileName,header = TRUE,sep=",");
   
   dataf <- data.frame(data_all);
+  #Remove NA's
+  dataf <- dataf[complete.cases(dataf),] 
   #summary(dataf)
   
   return(dataf);
 }
 
+
+#-------------------------------------------------------------
+#Scramble the dataset before extracting the training set.
+scrambleData<-function(dataf){
+  set.seed(8850);
+  g<- runif((nrow(dataf))); #generates a random distribution
+  dataf <- dataf[order(g),];
+  return (dataf);
+}
+
+#--------------------------------------------------------------
+#Extract the unique items from a column and return them sorted
+listUniqueItems<- function(column,columnName){
+  
+  #obtain a list of unique items
+  uniqueItems <- data.frame(unique(column));
+  colnames(uniqueItems) <- c(columnName);
+  
+  #Sort items in ascending order
+  uniqueItems <- uniqueItems[with(uniqueItems,order(columnName)),];
+  return(uniqueItems);
+}
