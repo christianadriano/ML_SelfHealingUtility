723b13a7b4c07ef2af7b86a249923a0468f6e9af
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-10-06 15:38:16 +0200

    removed features that are not used

--- PCA_UtilityFeatures_analysis.R
+++ PCA_UtilityFeatures_analysis.R
@@ -1,31 +1,38 @@
 #PCA Principal Component Analysis
+#Which software components are similar with regards to the principal components?
+#How the possible inputs to the utility function are related?
 
 install.packages("ir.pca");
 library (ir.pca);
 
 # load data
 source("C://Users//chris//OneDrive//Documentos//GitHub//ML_SelfHealingUtility//loadData.R");
-dataf<-loadData(fileName="MLDATA2_STATIC.csv");
+dataf<-loadData(fileName="MLDATA2_data.csv");
 summary(dataf);
 
-#-----------------------------------------------------------
 #Scramble the dataset before extracting the training set.
-set.seed(8850);
-g<- runif((nrow(dataf))); #generates a random distribution
-dataf <- dataf[order(g),];
+dataf <- scrambleData(dataf);
 
- #merge columns Failure.Name, Affected.Component, Rule
+#Remove all Failures that do not cause utility increase
+dataf<- dataf[dataf$FAILURE.NAME=="CF3",];
+
+#Select only the rows that have the Authentication component
+dataf<-dataf[grep("Auth", dataf$AFFECTED.COMPONENT), ]
+
+#merge columns Failure.Name, Affected.Component, Rule
 library(tidyr)
-dataf<-unite(dataf, FAILURE_RULE, c("FAILURE.NAME","RULE"), remove=FALSE);
-dataf<-unite(dataf, FAILURE_COMPONENT, c("FAILURE.NAME","AFFECTED.COMPONENT"), remove=FALSE);
+#dataf<-unite(dataf, FAILURE_RULE, c("FAILURE.NAME","RULE"), remove=FALSE);
+#dataf<-unite(dataf, FAILURE_COMPONENT, c("FAILURE.NAME","AFFECTED.COMPONENT"), remove=FALSE);
+
+#Create columns combinging two other columns
 dataf<-unite(dataf, RULE_COMPONENT, c("RULE","AFFECTED.COMPONENT") , remove=FALSE);
                
 # consider only the feature columns
-features_df<-data.frame(dataf$UTILITY.DROP,dataf$CRITICALITY,
-                        dataf$CONNECTIVITY,dataf$RELIABILITY);
-#                        dataf$DECISION);
+features_df<-data.frame(dataf$CRITICALITY,
+                        dataf$RELIABILITY);
+#                       dataf$CONNECTIVITY, dataf$UTILITY.DROP, dataf$DECISION);
 
-names<-c("Utility_Drop","Criticality","Connectity","Reliability");
+names<-c("Criticality","Reliability"); #"Utility_Drop","Connectity"
 colnames(features_df) <- names;
 
 # apply PCA - scale. = TRUE is highly 
@@ -75,10 +82,11 @@ plot_pca<-function(group_classes,pca_model){
 plot_pca(group_classes=dataf$FAILURE.NAME,pca_model=features_pca);
 plot_pca(group_classes=dataf$AFFECTED.COMPONENT,pca_model=features_pca);
 plot_pca(group_classes=dataf$RULE,pca_model=features_pca);
-plot_pca(group_classes=dataf$FAILURE_RULE,pca_model=features_pca);
-plot_pca(group_classes=dataf$RULE_COMPONENT,pca_model=features_pca);
-plot_pca(group_classes=dataf$FAILURE_COMPONENT,pca_model=features_pca);
-plot_pca(group_classes=dataf$COMPONENT,pca_model=features_pca);
+
+# plot_pca(group_classes=dataf$FAILURE_RULE,pca_model=features_pca);
+# plot_pca(group_classes=dataf$RULE_COMPONENT,pca_model=features_pca);
+# plot_pca(group_classes=dataf$FAILURE_COMPONENT,pca_model=features_pca);
+
 
 
 
