0241dd73e446d70e764081325ebaf0886716a9ff
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-11-09 21:10:48 +0100

    moved correl to another file

--- plotInputs.R
+++ plotInputs.R
@@ -54,36 +54,35 @@ library("Hmisc")
          y="Frequency")+
     scale_x_discrete(limits=c(min(dataf$CONNECTIVITY):max(dataf$CONNECTIVITY)));
 
-  #    
-  #Correlations
-  #Data is not normal (did not reject the null hypothesis)
-  shapiro.test(dataf$UTILITY.DROP);
-  shapiro.test(dataf$CRITICALITY);
-  shapiro.test(dataf$CONNECTIVITY);
-  shapiro.test(dataf$RELIABILITY);
-    
-  matrixInput<-data.frame(dataf$UTILITY.DROP,dataf$CRITICALITY,dataf$CONNECTIVITY,dataf$RELIABILITY);
-  colnames(matrixInput)<-c("Utility_Drop","Criticality","Connectivity","Reliability");
   
-  res <- rcorr(data.matrix(matrixInput),type=c("spearman"));
-  res$r
+###########################################################################
+# 
   
-  ##CORRELATIONS
-  #                Utility_Drop Criticality Connectivity Reliability
-  # Utility_Drop   1.00000000  0.87580705   0.41702881 -0.09647729
-  # Criticality    0.87580705  1.00000000  -0.05173893 -0.17340651
-  # Connectivity   0.41702881 -0.05173893   1.00000000  0.11248654
-  # Reliability   -0.09647729 -0.17340651   0.11248654  1.00000000
+  ggplot(data=dataf, aes(x=dataf$CONNECTIVITY, y=dataf$UTILITY.INCREASE)) +
+    geom_point() + geom_smooth(method="lm")+
+    ggtitle("Utility by CONNECTIVITY ")+
+    labs(x="Connectivity of the component", 
+         y="Utility Increase")+
+    scale_x_discrete(limits=c(min(dataf$CONNECTIVITY):max(dataf$CONNECTIVITY)));
   
-  ##p-values
-  res$P
-  #             Utility_Drop  Criticality Connectivity Reliability
-  # Utility_Drop           NA 2.508149e-11   0.01575973   0.5932733
-  # Criticality  2.508149e-11           NA   0.77491735   0.3345085
-  # Connectivity 1.575973e-02 7.749174e-01           NA   0.5331181
-  # Reliability  5.932733e-01 3.345085e-01   0.53311814          NA
+  ggplot(data=dataf, aes(x=dataf$CRITICALITY, y=dataf$UTILITY.INCREASE)) +
+    geom_point() + geom_smooth(method="lm")+
+    ggtitle("Utility by CRITICALITY ")+
+    labs(x="Criticality of the component", 
+         y="Utility Increase")+
+    scale_x_discrete(limits=c(min(dataf$CRITICALITY):max(dataf$CRITICALITY)));
+  
+  ggplot(data=dataf, aes(x=dataf$RELIABILITY, y=dataf$UTILITY.INCREASE)) +
+    geom_point() + geom_smooth(method="lm")+
+    ggtitle("Utility by RELIABILITY ")+
+    labs(x="Reliability of the component", 
+         y="Utility Increase");
+    scale_x_discrete(limits=c(min(dataf$RELIABILITY):max(dataf$RELIABILITY)));
   
   
+###########################################################################  
+# Normality test
+
 qqnorm(dataf$CRITICALITY,main="Normal Q-Q Plot - Criticality")+ qqline();
 qqline(dataf$CRITICALITY)
   
