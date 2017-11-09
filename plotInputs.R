259fbf00dae66fff594a0f540560fbe83b3583df
Christian Medeiros Adriano <christian.adriano@gmail.com>
2017-11-09 17:28:53 +0100

    Compute p-values for correlations

--- plotInputs.R
+++ plotInputs.R
@@ -3,22 +3,29 @@
 #How are the features correlated? Yes, medium to highly correlated.
 
 library(ggplot2)
+install.packages("Hmisc")
+library("Hmisc")
 
   # load data
   source("C://Users//chris//OneDrive//Documentos//GitHub//ML_SelfHealingUtility//loadData.R");
   dataf<-loadData(fileName="Random_proper_comp_names.csv");
+  
+  #Remove all reliability and utility values equal to zero
+  dataf<- dataf[dataf$RELIABILITY!=0,];
+  dataf <- dataf[dataf$UTILITY.INCREASE!=0,] 
 
   #validationf<- loadData(fileName = "MLDATA2_STATIC.csv")
 
   summary(dataf);
   
-  ggplot(data=dataf, aes(x=dataf$UTILITY.DROP)) +
+  ggplot(data=dataf, aes(x=dataf$UTILITY.INCREASE)) +
     geom_bar(alpha=1, position="identity")+
-    geom_vline(aes(xintercept=mean(dataf$UTILITY.DROP, na.rm=T)),   # Ignore NA values for mean
+    geom_vline(aes(xintercept=mean(dataf$UTILITY.INCREASE, na.rm=T)),   # Ignore NA values for mean
                color="red", linetype="dashed", size=1) +
-    ggtitle("Utility drop distribution")+
-    labs(x="Utility drop from disconnecting a component", 
+    ggtitle("Utility increase distribution")+
+    labs(x="Utility increase from disconnecting a component", 
          y="Frequency");
+#    scale_x_discrete(limits=c(min(dataf$UTILITY.INCREASE):max(dataf$UTILITY.INCREASE)));
   
   ggplot(data=dataf, aes(x=dataf$CRITICALITY)) +
     geom_bar(alpha=.2, position="identity")+
@@ -58,15 +65,23 @@ library(ggplot2)
   matrixInput<-data.frame(dataf$UTILITY.DROP,dataf$CRITICALITY,dataf$CONNECTIVITY,dataf$RELIABILITY);
   colnames(matrixInput)<-c("Utility_Drop","Criticality","Connectivity","Reliability");
   
-  cor(matrixInput, method="kendall",use="pairwise");
-#                Utility_Drop Criticality Connectivity  Reliability
-# Utility_Drop   1.00000000   0.217679117  0.49190185  -0.032406079
-# Criticality    0.21767912   1.000000000  0.06472386   0.002819633
-# Connectivity   0.49190185   0.064723861  1.00000000  -0.053289824
-# Reliability   -0.03240608   0.002819633 -0.05328982   1.000000000
+  res <- rcorr(data.matrix(matrixInput),type=c("spearman"));
+  res$r
   
-  #As we can see features (connectivity, criticality, reliabiliyt) 
-  #are medium to low correlated
+  ##CORRELATIONS
+  #                Utility_Drop Criticality Connectivity Reliability
+  # Utility_Drop   1.00000000  0.87580705   0.41702881 -0.09647729
+  # Criticality    0.87580705  1.00000000  -0.05173893 -0.17340651
+  # Connectivity   0.41702881 -0.05173893   1.00000000  0.11248654
+  # Reliability   -0.09647729 -0.17340651   0.11248654  1.00000000
+  
+  ##p-values
+  res$P
+  #             Utility_Drop  Criticality Connectivity Reliability
+  # Utility_Drop           NA 2.508149e-11   0.01575973   0.5932733
+  # Criticality  2.508149e-11           NA   0.77491735   0.3345085
+  # Connectivity 1.575973e-02 7.749174e-01           NA   0.5331181
+  # Reliability  5.932733e-01 3.345085e-01   0.53311814          NA
   
   
 qqnorm(dataf$CRITICALITY,main="Normal Q-Q Plot - Criticality")+ qqline();
