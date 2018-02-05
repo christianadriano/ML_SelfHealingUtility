7c2d2551aa161f92e21339373621114b51617624
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-02-06 00:38:14 +0100

    folder update

--- loadData.R
+++ loadData.R
@@ -4,7 +4,7 @@
 #Load all data into a dataframe
 loadData<- function(fileName){
   
-  setwd("C://Users//chris//OneDrive//Documentos//GitHub//ML_SelfHealingUtility//");
+  setwd("C://Users//Chris//Documents//GitHub//ML_SelfHealingUtility//");
   
   data_all <- read.csv(fileName,header = TRUE,sep=",");
   
