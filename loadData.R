02c9960ade249fc25ac280814d977e2cc8f120f4
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-03-30 16:40:51 +0200

    move generic functions to be reused

--- loadData.R
+++ loadData.R
@@ -196,3 +196,89 @@ averageRMSE <- function(RMSEVector, sampleSize){
   return (sum(RMSE_sqr /(RMSE_points * sampleSize)))
 }
 
+# Save results to file ----------------------------------------------------
+resultsToFile <- function(mcResults,modelName,extension){
+  fileName <- paste0("mcResultsf_",modelName,extension);
+  write.table(mcResults,fileName,sep=",",col.names = TRUE);
+  return (paste0("file written:",fileName));
+}
+
+# Generate the dataset names that will be trained -------------------------
+generateDataSetNames <- function(modelName,datasetSize,s_idx){
+  
+  if(s_idx==0 & length(datasetSize)>0){#Generate for all sizes
+    datasetName <- paste0(modelName,datasetSize[1]);
+    for(i in c(2:length(datasetSize))){
+      datasetName <- cbind(datasetName,paste0(modelName,datasetSize[i]));
+    }
+  }
+  else{
+    datasetName <- paste0(modelName,datasetSize[s_idx]);
+  }
+  return(datasetName);
+}
+
+# Prepare features --------------------------------------------------------
+prepareFeatures <- function(dataf,selectionType){
+  
+  #Do feature selection (or not)
+  if(selectionType=="ALL")
+    featuresdf<- select_ALL(dataf) 
+  else
+    if(selectionType=="Linear")
+      featuresdf<- select_Linear(dataf) 
+    else
+      if(selectionType=="Discontinuous")
+        featuresdf<- select_Discontinuous(dataf) 
+      else
+        if(selectionType=="Saturating")
+          featuresdf<- select_Saturation(dataf) 
+        
+        #Remove zero utilities
+        featuresdf <- featuresdf[featuresdf$UTILITY_INCREASE!=0,];
+        
+        # Scramble data 
+        featuresdf <- scrambleData(datadf=featuresdf);
+        
+        return (featuresdf);
+}
+
+
+# Generate PMML file ------------------------------------------------------
+
+generatePMML <- function(model, featuresdf,modelName){  
+  
+  inputFeatures <- dim(featuresdf)[2] - 1; #last column is the target variable
+  
+  # Generate feature map
+  xgboost.fmap = r2pmml::genFMap(featuresdf[1:inputFeatures])
+  r2pmml::writeFMap(xgboost.fmap, "xgboost.fmap")
+  
+  # Save the model in XGBoost proprietary binary format
+  xgb.save(model, "xgboost.model")
+  
+  # Dump the model in text format
+  #  xgb.dump(model, "xgboost.model.txt", fmap = "xgboost.fmap");
+  
+  pmmlFileName <- paste0(".//pmml///",modelName,"-xgb.pmml");
+  
+  r2pmml(model, pmmlFileName, fmap = xgboost.fmap, response_name = "UTILITY_INCREASE", 
+         missing = NULL, ntreelimit = 25, compact = TRUE)
+}
+
+converTimeToDataFrame <- function(time_elapsed){
+  
+  frame <- data.frame(as.matrix(time_elapsed));
+  user.self <- frame$mm[1];
+  sys.self <- frame$mm[2];
+  elapsed.self <- frame$mm[3];
+  
+  time.df$user.self <- user.self;
+  time.df$sys.self <- sys.self;
+  time.df$elapsed.self <- elapsed.self;
+  
+  return (time.df);
+}
+
+
+
