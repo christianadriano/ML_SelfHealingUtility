07a6ff1e971e27c0f739d34bb40a11cdd9cdaf30
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-04-03 11:33:26 +0200

    parameterized training call

--- models/gbmRegression.R
+++ models/gbmRegression.R
@@ -28,11 +28,11 @@ trainGBM <- function(training.df,numberOfTrees=2500,kfolds=10){
     trained.model <- gbm(UTILITY_INCREASE~.,
                          data = training.df,
                          distribution = "gaussian",
-                         n.trees = 2500,
+                         n.trees = numberOfTrees,
                          n.minobsinnode = 100,
                          shrinkage = 0.01,
                          bag.fraction = 0.5,
-                         cv.folds = 10)
+                         cv.folds = kfolds)
   )
   
   best.iteration = gbm.perf(trained.model, method = "cv")
@@ -46,30 +46,29 @@ trainGBM <- function(training.df,numberOfTrees=2500,kfolds=10){
 }
 
 # Validation -------------------------------------------------------------
-validateGBM <- function(outcome.list,validation.data,i){
+validateGBM <- function(outcome.list,validation.df,i){
   
   results.df <- data.frame(matrix(data=NA,nrow=3,ncol=12));
-  colnames(results.df) <- c("Item","Utility_Type","Train_RMSE_MEAN","Train_RMSE_STD","Test_RMSE_MEAN",
-                            "Test_RMSE_STD","RMSE","R_Squared", "MAPD","User_Time","Sys_Time","Elapsed_Time");
+  colnames(results.df) <- c("Item","Utility_Type","RMSE","R_Squared", "MAPD","User_Time","Sys_Time","Elapsed_Time");
 
   trained.model <- outcome.list[[1]];
   time.df <- outcome.list[[2]]
   
   best.iteration <- trained.model$best_iteration;
   
-  y_pred <- predict(trained.model, validation.data, n.trees = best.iteration);
-  error <- y_pred - validation.data$UTILITY_INCREASE;
+  y_pred <- predict(trained.model, validation.df);
+  error <- y_pred - validation.df$UTILITY_INCREASE;
   
   results.df$Item[i] <- i;
   results.df$Utility_Type[i]<-gsub(" ","",datasetName[i],fixed = TRUE);
-  results.df$Train_RMSE_MEAN[i]<-trained.model$evaluation_log[best.iteration]$train_rmse_mean;
-  results.df$Train_RMSE_STD[i]<-trained.model$evaluation_log[best.iteration]$train_rmse_std;
-  results.df$Test_RMSE_MEAN[i]<-trained.model$evaluation_log[best.iteration]$test_rmse_mean;
-  results.df$Test_RMSE_STD[i]<-trained.model$evaluation_log[best.iteration]$test_rmse_std;
+  # results.df$Train_RMSE_MEAN[i]<-trained.model$evaluation_log[best.iteration]$train_rmse_mean;
+  # results.df$Train_RMSE_STD[i]<-trained.model$evaluation_log[best.iteration]$train_rmse_std;
+  # results.df$Test_RMSE_MEAN[i]<-trained.model$evaluation_log[best.iteration]$test_rmse_mean;
+  # results.df$Test_RMSE_STD[i]<-trained.model$evaluation_log[best.iteration]$test_rmse_std;
   
   results.df$RMSE[i] <- rmse(error);
-  results.df$R_Squared[i] <- r_squared(y_pred,validation.data$UTILITY_INCREASE);
-  results.df$MAPD[i] <- mapd(y_pred,validation.data$UTILITY_INCREASE);
+  results.df$R_Squared[i] <- r_squared(y_pred,validation.df$UTILITY_INCREASE);
+  results.df$MAPD[i] <- mapd(y_pred,validation.df$UTILITY_INCREASE);
   
   results.df$User_Time[i] <- time.df$user.time;
   results.df$Sys_Time[i] <- time.df$sys.time;
