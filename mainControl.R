5623f1f9763a1fa0683ef08f89a7c91e01f5c9c9
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-04-04 22:46:02 +0200

    structure to compute GBM

--- mainControl.R
+++ mainControl.R
@@ -17,51 +17,49 @@ source("C://Users//Chris//Documents//GitHub//ML_SelfHealingUtility//loadData.R")
 source("C://Users//Chris//Documents//GitHub//ML_SelfHealingUtility//models//xboostRegression.R");
 source("C://Users//Chris//Documents//GitHub//ML_SelfHealingUtility//models//gbmRegression.R");
 
-  
-  #Data structure to keep results
- 
-  #Folder with training data
-  folder <- "C://Users//Chris//Documents//GitHub//ML_SelfHealingUtility//data//DataPoints_1K-3K-9K//";
-  #folder <- "//DataPoints_1K-3K-9K//";
-  
-  # CONTROL CODE   ------------------------------------------------------------
-  
-  model.name <- c("Linear","Discontinuous","Saturating","ALL")[1];
-  
-  method.name <- c("GBM","XGBoost","LigthGBM")[1];
-  
-  dataset.name.list <- generateDataSetNames(model.name, c("1K","3K","9K"),0);
 
-  results.df <- data.frame(matrix(data=NA,nrow=3,ncol=8));
-  colnames(results.df) <- c("Item","Utility_Type","RMSE","R_Squared", "MAPD","User_Time","Sys_Time","Elapsed_Time");
+#Data structure to keep results
+
+#Folder with training data
+folder <- "C://Users//Chris//Documents//GitHub//ML_SelfHealingUtility//data//DataPoints_1K-3K-9K//";
+#folder <- "//DataPoints_1K-3K-9K//";
+
+# CONTROL CODE   ------------------------------------------------------------
+
+model.name <- c("Linear","Discontinuous","Saturating","ALL")[4];
+
+method.name <- c("GBM","XGBoost","LigthGBM")[1];
+
+dataset.name.list <- generateDataSetNames(model.name, c("1K","3K","9K"),0);
+
+results.df <- data.frame(matrix(data=NA,nrow=3,ncol=9));
+colnames(results.df) <- c("Item","Utility_Type","RMSE","R_Squared", "MAPD","User_Time","Sys_Time","Elapsed_Time","Number_of_Trees");
+
+for(i in c(1:length(dataset.name.list))){
+  fileName <- paste0(folder,dataset.name.list[i],".csv");
+  data.df <- loadData(fileName);
+  #data_all <- read.csv(fileName,header = TRUE,sep=",");
   
-  for(i in c(1:length(dataset.name.list))){
-    # i <- 1;
-    fileName <- paste0(folder,dataset.name.list[i],".csv");
-    data.df <- loadData(fileName);
-    #data_all <- read.csv(fileName,header = TRUE,sep=",");
-    
-    features.df <- prepareFeatures(data.df,"Saturating");
-    
-    #Extract training ad validation sets 
-    totalData.size <- dim(features.df)[1];
-    training.size <- trunc(totalData.size * 0.7);
-    startTestIndex <- totalData.size - training.size;
-    training.df <- as.data.frame(features.df[1:training.size,]);
-    validation.df <-as.data.frame(features.df[startTestIndex:totalData.size,]);
-
-    #Train model  
-    numberOfTrees=2500
-    outcome.list <- trainGBM(training.df,numberOfTrees,kfolds=10);
-
-    #Validate model
-    results.df <- validateGBM(outcome.list,validation.df,dataset.name.list,i,results.df);
-  }
-
+  features.df <- prepareFeatures(data.df,"ALL");
   
+  #Extract training ad validation sets 
+  totalData.size <- dim(features.df)[1];
+  training.size <- trunc(totalData.size * 0.7);
+  startTestIndex <- totalData.size - training.size;
+  training.df <- as.data.frame(features.df[1:training.size,]);
+  validation.df <-as.data.frame(features.df[startTestIndex:totalData.size,]);
+  
+  #Train model
+  outcome.list <- trainGBM(training.df,numberOfTrees=15000,kfolds=10);
+  
+  #Validate model
+  results.df <- validateGBM(outcome.list,validation.df,dataset.name.list,i,results.df);
+}
+
+
 #print(results.df); #show on the console
 
-message <- resultsToFile(results.df,model.name,"_70-30_NOFeatureSelection.csv"); #save to a .csv file
+message <- resultsToFile(results.df,model.name,method.name,"_70-30_NOFeatureSelection.csv"); #save to a .csv file
 print(message);
 
 pmmlFileName <- paste0(".//pmml///",dataset.name.list[i],"-",method.name,".pmml");
