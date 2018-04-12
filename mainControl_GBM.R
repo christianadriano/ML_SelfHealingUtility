fde80e0ea7f1e29e040295f0b951d9b542836e06
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-04-12 22:47:50 +0200

    rerun with more trees

--- mainControl_GBM.R
+++ mainControl_GBM.R
@@ -27,20 +27,20 @@ model.name <- model.name.list[4];
 
 method.name <- "GBM";
 
-dataset.name.list <- generateDataSetNames(model.name, c("1K","3K","9K"),0);
+dataset.name.list <- generateDataSetNames(model.name, c("9k"),0);
 
 results.df <- data.frame(matrix(data=NA,nrow=1000,ncol=14));
-colnames(results.df) <- c("Item","Utility_Type","RMSE","R_Squared", "MAPD","User_Time","Sys_Time","Elapsed_Time",
+colnames(results.df) <- c("Item","Utility_Type","RMSE","R_Squared", "MADP","User_Time","Sys_Time","Elapsed_Time",
                           "Number_of_Trees","Learning_Rate","Max_Depth","Train_Split","Min_Data_In_Leaf",
                           "Bagging_Fraction");
 
 results_line <- 0;
 
 for(model.name in model.name.list){
-  dataset.name.list <- generateDataSetNames(model.name, c("1K","3K","9K"),0);
+  dataset.name.list <- generateDataSetNames(model.name, c("9K"),0);
   
   for(i in c(1:length(dataset.name.list))){
-    #i <- 3;
+    i <- 1;
     results_line <- results_line+1;
     
     fileName <- paste0(folder,dataset.name.list[i],".csv");
@@ -56,7 +56,7 @@ for(model.name in model.name.list){
     validation.df <- as.data.frame(features.df[training.size:totalData.size,]);
     
     #Train model
-    numberOfTrees <- 5000;
+    numberOfTrees <- 10000;
     kfolds <- 10;
     learning.rate <- 0.01;
     min.nodes.in.leaf <- 5;
@@ -68,10 +68,10 @@ for(model.name in model.name.list){
     results.df <- validate_GBM(outcome.list,validation.df,dataset.name.list[i],results_line,results.df,
                                learning.rate,min.nodes.in.leaf);
     
-    message <- resultsToFile(results.df,model.name,method.name,"_70-30_NOFeatureSelection.csv"); #save to a .csv file
+    message <- resultsToFile(results.df,model.name,method.name,"_70-30_NOFeatureSelection_9K15K.csv"); #save to a .csv file
     print(message);
     
-    pmmlFileName <- paste0(".//pmml///",dataset.name.list[i],"-5KTrees_learnRate",method.name,".pmml");
+    pmmlFileName <- paste0(".//pmml///",dataset.name.list[i],"-15KTrees",method.name,".pmml");
     generatePMML(outcome.list[[1]],training.df,pmmlFileName,numberOfTrees);
     
   }
